fd28bb6c8a0b8c85af9bf8266223f38f6a0153fd
Neutrino Kusrorong <kneotrino@yahoo.com>
2019-10-09 14:36:32 +0700

    Init Project

---
+++ To Forget List/To Forget List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Forget List
+//
+//  Created by Fintopia Indonesia on 09/10/19.
+//  Copyright © 2019 Clay4Seed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
